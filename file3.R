df37f2d3cc3b2190ebe887b3ef4422954dc1d959
RohanZutshi <zutshirohan@gmail.com>
2018-11-16 18:51:18 +0530

    a

---
+++ file3.R
@@ -0,0 +1,26 @@
+x1=3 #assign
+x2<-3 #same
+x2
+ls() #var in environment-listing
+y=8
+women
+data() #shows the datasets available for use
+?AirPassengers #shows details of data- data dictionary
+library()
+?mean #help
+help(mean)
+??mean #search through other sources
+x <- c(0:10, 50)
+x
+xm <- mean(x)
+c(xm, mean(x, trim = 0.10)) #trimmed 10% marks from both extreme ends and then mean is calculated
+1:1000000
+x<- c(0:10,50)
+x
+?c #combine
+length(x)
+xm
+c
+Sys.Date()
+getwd()
+plot(1:10) 
